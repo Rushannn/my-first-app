e1b6d1ea890183e27b556f3d3621d11b31b257a0
Rushannn <rushankarimov@gmail.com>
2024-01-19 22:58:33 +0300

    add token inteceptor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ITodo } from './core/models/ITodo.model';
 import { TodoService } from './core/services/todo.service';
 import { Subject, takeUntil } from 'rxjs';
+import { AuthState } from './core/services/auth.state';
 
 
 
@@ -10,7 +11,22 @@ import { Subject, takeUntil } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-first-app';
+
+  constructor(
+    private authState: AuthState
+  ) { }
+
+
+  ngOnInit(): void {
+    const token: string | undefined | null = localStorage.getItem('token')
+    if (token) {
+      this.authState.authTokenSubject.next(token);
+      console.log('ngOnInit',token)
+    }
+  }
+
+
 
 }
--- src/app/core/services/auth.state.ts
+++ src/app/core/services/auth.state.ts
@@ -1,17 +1,31 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject } from "rxjs";
 import { AuthService } from "./auth.service";
+import { IAuth } from "../models/IAuth.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthState {
 
-  private authTokenSubject = new BehaviorSubject<string | null>(null);
+  public authTokenSubject = new BehaviorSubject<string | null>(null);
 
   constructor(
     private authService: AuthService
-  ) {  }
+  ) { }
 
-
+  signup(credentials: IAuth) {
+    console.log('AuthState signup')
+    this.authService.signUp(credentials)
+      .subscribe({
+        next: ({ authToken }) => {
+          console.log('token', authToken);
+          this.authTokenSubject.next(authToken);
+          localStorage.setItem('token', authToken);
+        },
+        error: (err) => {
+          console.error('Signup error', err)
+        }
+      })
+  }
 }
